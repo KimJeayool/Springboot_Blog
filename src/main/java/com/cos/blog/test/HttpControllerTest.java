ed9a72159f8df6293e7c2fafef97468e42199a7c
jaeyoolkim <s09c073@gmail.com>
2023-07-04 23:41:43 +0900

    # Http 요청 테스트 진행

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -8,18 +8,18 @@ import org.springframework.web.bind.annotation.*;
 public class HttpControllerTest {
 
     @GetMapping("/http/get")
-    public String getTest() {
-        return "GET 요청";
+    public String getTest(@RequestParam int id, @RequestParam String username, Member member) { // MessageConverter (스프링부트)
+        return "GET 요청 : " + id + username + member.getUsername() + member.getEmail();
     }
 
-    @PostMapping("/http/post")
-    public String postTest() {
-        return "POST 요청";
+    @PostMapping("/http/post") // text/plain, application/json
+    public String postTest(@RequestBody Member member) { // MessageConverter (스프링부트)
+        return "POST 요청 : " + member.toString();
     }
 
     @PutMapping("/http/put")
-    public String putTest() {
-        return "PUT 요청";
+    public String putTest(@RequestBody Member member) {
+        return "PUT 요청 : " + member.toString();
     }
 
     @DeleteMapping("/http/delete")
